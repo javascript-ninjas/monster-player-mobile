0af8b5954f8072b88817a475722ca02b4d7ad469
Adam Miniuk <adam.miniuk@cyfrowypolsat.pl>
2015-09-26 13:59:40 +0200

    add CoffeScript

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var coffee = require('gulp-coffee');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  coffee: ['./www/**/*.coffee']
 };
 
 gulp.task('default', ['sass']);
@@ -27,8 +29,18 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+gulp.task('coffee', function(done) {
+  gulp.src(paths.coffee)
+    .pipe(coffee({bare: true})
+      .on('error', gutil.log.bind(gutil, 'Coffee Error')))
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./www/js'))
+    .on('end', done)
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.coffee, ['coffee'])
 });
 
 gulp.task('install', ['git-check'], function() {
---
+++ www/js/app.js
@@ -0,0 +1,106 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services']).run(function($ionicPlatform) {
+  return $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      return StatusBar.styleLightContent();
+    }
+  });
+}).config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  }).state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  }).state('tab.chats', {
+    url: '/chats',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/tab-chats.html',
+        controller: 'ChatsCtrl'
+      }
+    }
+  }).state('tab.chat-detail', {
+    url: '/chats/:chatId',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
+      }
+    }
+  }).state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+  return $urlRouterProvider.otherwise('/tab/dash');
+});
+
+angular.module('starter.controllers', []).controller('DashCtrl', function($scope) {}).controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  return $scope.remove = function(chat) {
+    return Chats.remove(chat);
+  };
+}).controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  return $scope.chat = Chats.get($stateParams.chatId);
+}).controller('AccountCtrl', function($scope) {
+  return $scope.settings = {
+    enableFriends: true
+  };
+});
+
+angular.module('starter.services', []).factory('Chats', function() {
+  var chats;
+  chats = [
+    {
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }
+  ];
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      return chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      return null;
+    }
+  };
+});
